eb7a1e7e458dd5a9b69f4838938ad924abc2271d
thajunnisaph <thajunnisaph123@gmail.com>
2022-11-09 17:20:49 +0530

    use useReduser for password and email validation,Value

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, {useState,Fragment} from 'react';
-import AddUser from './Components/Users/AddUser';
-import './index.css';
-import UserList from './Components/Users/UserList';
+import React, { useState, useEffect } from 'react';
 
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-   const [UsersList,SetUserList]=useState([]);
-  const UserHandler=(UName,Age,Clg) =>{
- SetUserList((prevState) =>{
-return [...prevState,{name:UName, age:Age, clgname:Clg, id:UName+Age}];
- })
-  }
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
   return (
-    <Fragment>
-     <AddUser  AddUser={UserHandler}/>
-     <UserList  Users={UsersList} /> 
-    </Fragment>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 

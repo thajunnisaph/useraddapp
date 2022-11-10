cecdab21979a795109985e7a830416e45bfce510
thajunnisaph <thajunnisaph123@gmail.com>
2022-11-10 20:18:13 +0530

    create a reusable Input component which could replace the current input elements which take email and password. The new component should take all the data as props.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
-
-const Home = (props) => {
+import Button from '../UI/Button/Button';
+import AuthContext from '../../store/AuthContext';
+const Home = () => {
+   const ctx1 = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={ctx1.onLogout}>Logout</Button>
     </Card>
   );
 };
